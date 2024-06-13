6406468f4f078930db0ae58cf480d2f3f55be0de
Shrikant Yadav <shrikant9907@gmail.com>
2024-06-13 18:45:24 +0530

    Deploy on github pages

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/reactjs-todo-list-html-css',
   server: {
     port: 3000
   }
